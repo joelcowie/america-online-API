258c94c7bcf8307738831cb89437126e6753ecf6
aemrich <adam.emrich@gmail.com>
2017-02-15 15:58:26 -0500

    added park api

--- db/migrate/20170214182320_create_parks.rb
+++ db/migrate/20170214182320_create_parks.rb
@@ -2,7 +2,13 @@ class CreateParks < ActiveRecord::Migration[5.0]
   def change
     create_table :parks do |t|
       t.string :name
-      t.string :location
+      t.string :states
+      t.string :latLong
+      t.string :description
+      t.string :directionsInfo
+      t.string :directionsUrl
+      t.string :url
+      t.string :weatherInfo
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,9 +7,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+adapter = NationalParkAdapter.new
+adapter.populate_park_data
 
+# array = JSON.parse(File.open('/Users/adam/Development/Flatiron/Immersive/react/finalproject/roadtripper-API/national_park_hash.rb'))
+# puts array[0][1]
+# array.collect do |parks|
+#   # puts parks[0]
+#   Park.create(name: parks['fullName'])
+# end
 
-Park.create(name:"Glacier National Park", location:"Montana")
-Park.create(name:"Yellowstone National Park", location:"Wyoming")
-Park.create(name:"Yosemite National Park", location: "California")
-Park.create(name:"Zion National Park", location: "Utah")
+# Park.create(name:"Glacier National Park", location:"Montana")
+# Park.create(name:"Yellowstone National Park", location:"Wyoming")
+# Park.create(name:"Yosemite National Park", location: "California")
+# Park.create(name:"Zion National Park", location: "Utah")
